6e401ebfa0369bc7cdbec3f1e072d575a2aa8016
Mario Carrillo <mcarrillo@cedi.edu.mx>
2024-04-29 13:21:39 -0600

    Revert "finished EAS01"
    This reverts commit 5488466aab99874df93fc558c62d38d631619d90.

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -6,16 +6,12 @@ using namespace std;
 Rectangle::Rectangle(Vector2f size)
 {
     /**  (4pts)
-        Inicializa las variables:
+        Inicializa las variables: 
         - shape: un rectángulo de tamaño size.
         - speed: un Vector2f con valores (0, 0).
         - objective: un Vector2f con valores (0, 0).
         - Haz que el cuadrado se pinte de un color.
     */
-    this->shape = RectangleShape(size);
-    this->speed = Vector2f(0, 0);
-    this->objective = Vector2f(0, 0);
-    this->shape.setFillColor(Color::Green);
 }
 
 Rectangle::Rectangle(Vector2f size, Vector2i position)
@@ -28,11 +24,6 @@ Rectangle::Rectangle(Vector2f size, Vector2i position)
         - Haz que el cuadrado se pinte de un color.
         - Haz que el cuadrado esté en la posición position.
     */
-    this->shape = RectangleShape(size);
-    this->speed = Vector2f(0, 0);
-    this->objective = Vector2f(position);
-    this->shape.setFillColor(Color::Green);
-    this->shape.setPosition(Vector2f(position));
 }
 
 void Rectangle::update()
@@ -43,27 +34,10 @@ void Rectangle::update()
         Si no, píntalo de rojo.
             - Puedes saber si está a 5 pixeles del objetivo si la diferencia entre la coordeanda x del cuadrado y la x del objetivo es menor a 5 y lo mismo con la y.
     */
-    this->shape.move(this->speed);
-    if (abs(this->objective.x - this->shape.getPosition().x) < 5 &&
-        abs(this->objective.y - this->shape.getPosition().y) < 5)
-    {
-        this->speed = Vector2f(0, 0);
-        this->shape.setPosition(this->objective);
-        this->shape.setFillColor(Color::Green);
-    }
-    else
-    {
-        this->shape.setFillColor(Color::Red);
-    }
 }
 
 void Rectangle::setObjective(Vector2f objective)
 {
-    this->objective = objective - Vector2f(this->shape.getSize().x/2,this->shape.getSize().y/2);
-    Vector2f direccion = this->objective - this->shape.getPosition();
-    float magnitud = sqrt(pow(direccion.x,2) + pow(direccion.y,2));
-    this->speed.x = direccion.x/magnitud * 5.f;
-    this->speed.y = direccion.y/magnitud * 5.f;
     /**(5pts)
         Asigna objetivo a la propiedad objective.
         Calcula la dirección hacia el objetivo y la velocidad necesaria para llegar al objetivo:
